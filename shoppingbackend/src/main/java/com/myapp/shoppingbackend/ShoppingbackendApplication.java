ecf6f80c8aebbed182d941f5fd64e38b0a2576c0
ESUM BRANDON <esumbrandon074@gmail.com>
2024-04-08 18:41:45 +0100

    backend services update

---
+++ shoppingbackend/src/main/java/com/myapp/shoppingbackend/ShoppingbackendApplication.java
@@ -0,0 +1,13 @@
+package com.myapp.shoppingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingbackendApplication.class, args);
+	}
+
+}
---
+++ shoppingbackend/src/test/java/com/myapp/shoppingbackend/ShoppingbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myapp.shoppingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
