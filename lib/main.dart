2009f36562b04c32387623fbbe7735f95fd33296
ESUM BRANDON <esumbrandon074@gmail.com>
2024-04-10 14:56:47 +0100

    Reconfiguration of the main file to fit the implementation of firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,26 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:shop_app/providers/cart_provider.dart';
 import 'package:shop_app/pages/home_page.dart';
+import 'package:shop_app/providers/cart_provider.dart';
 
-void main() {
+
+void main() async {
+WidgetsFlutterBinding.ensureInitialized();
+
+  if (kIsWeb) {
+   await Firebase.initializeApp(options: const FirebaseOptions(apiKey: "AIzaSyAI7inSHSrNAO_51053vYgPJG0Lk6F5-vs",
+  authDomain: "market-place-16881.firebaseapp.com",
+  projectId: "market-place-16881",
+  storageBucket: "market-place-16881.appspot.com",
+  messagingSenderId: "825574940956",
+  appId: "1:825574940956:web:782d134c57f6fa71475ad9",
+  measurementId: "G-MG457204BZ"));
+  } else {
+   await Firebase.initializeApp();
+  }
+
   runApp(const MyApp());
 }
 
